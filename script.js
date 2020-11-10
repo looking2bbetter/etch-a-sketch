cb92962eda00d9a87a42c03fad09c4a81982a146
looking2bbetter <jtburroughs1998@yahoo.com>
2020-11-10 13:00:53 -0800

    fixed it so that the divs still become colorized even after resetting and creating a new grid

--- script.js
+++ script.js
@@ -3,22 +3,25 @@ const container = document.querySelector('#container');
 let gridSize = 25;
 
 let gridCreate = () => {
-    console.log(gridSize);
+    
     for (let i = 1; i <= gridSize * gridSize; i++) {
         container.innerHTML += "<div class='divis'><p>" + i + "</p></div>";
 
         container.setAttribute('style', 'width: ' + gridSize * 44 + 'px;');
+
+
     }
-}
 
-gridCreate();
+    const divis = document.querySelectorAll('.divis');
 
-const divis = document.querySelectorAll('.divis');
-divis.forEach((div) => {
+    divis.forEach((div) => {
         div.addEventListener('mouseenter', () => {
             div.setAttribute('style', 'background-color: blue;');
         })
     });
+}
+
+gridCreate();
 
 let button = document.querySelector('button');
 
@@ -30,8 +33,8 @@ button.addEventListener('click', () => {
     let requestedGridSize = prompt("How large of a grid would you like?");
 
     gridSize = requestedGridSize;
-    console.log(gridSize);
+
     container.innerHTML = "";
+
     gridCreate();
-    console.log(gridSize);
 })
